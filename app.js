61eff663e832fef2c81c657a84b78abad15ea976
Xchel_Sanchez <xchelsvz@gmail.com>
2016-01-29 11:33:01 -0600

    Adding Menu Views and Streaming Simulation

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.get('/', routes.index);
 app.get('/teleferico' ,function (request,response){
     response.render('teleferico')
 })
+app.get('/parquePaseo' ,function (request,response){
+    response.render('parque')
+})
+app.get('/streaming' ,function (request,response){
+    response.render('streaming')
+})
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
